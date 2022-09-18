7b89954e6749c9baa0a0eb7d41c9e2f448e00cac
Andreas Trøstrup <andreastrstrup@gmail.com>
2022-09-18 14:08:16 +0200

    implement filter and flatten

--- Assignment2/Extensions.cs
+++ Assignment2/Extensions.cs
@@ -4,12 +4,24 @@ public static class Extensions
 {
     public static IEnumerable<T> Filter<T>(this IEnumerable<T> items, Predicate<T> predicate)
     {
-        throw new NotImplementedException();
+        foreach (var item in items)
+        {
+            if (predicate(item))
+            {
+                yield return item;
+            }
+        }
     }
 
     public static IEnumerable<T> Flatten<T>(this IEnumerable<IEnumerable<T>> items)
     {
-        throw new NotImplementedException();
+        foreach (var item in items)
+        {
+            foreach (var i in item)
+            {
+                yield return i;
+            }
+        }
     }
 
     public static bool IsSecure(this Uri uri)
