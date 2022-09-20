f3b9f03881e71d4f25e979461efb512242386f68
oljh <oljh@itu.dk>
2022-09-20 12:37:11 +0200

    ImmutableStudent tested and implemented v2

--- Assignment2.Tests/ImmutableStudentTests.cs
+++ Assignment2.Tests/ImmutableStudentTests.cs
@@ -6,16 +6,16 @@ public class ImmutableStudentTests
     public void ToString_dropout()
     {
         //Arrange
-        var actual = new ImmutableStudent(
-            id: 1,
-            givenname: "Jens",
-            surname: "Jensen",
-            startdate: new DateTime(2019, 8, 23),
-            enddate: new DateTime(2021, 6, 16),
-            graduationdate: new DateTime(2022, 6, 16)
+        var student = new ImmutableStudent(
+            1,
+            "Jens",
+            "Jensen",
+            new DateTime(2019, 8, 23),
+            new DateTime(2021, 6, 16),
+            new DateTime(2022, 6, 16)
         );
         //Assert
-        actual.ToString().Should().Be("ImmutableStudent { Id = 1, GivenName = Jens, Surname = Jensen, Status = Dropout, StartDate = 23-08-2019 00:00:00, EndDate = 16-06-2021 00:00:00, GraduationDate = 16-06-2022 00:00:00 }");
+        student.ToString().Should().Be("ImmutableStudent { Id = 1, GivenName = Jens, Surname = Jensen, StartDate = 23-08-2019 00:00:00, EndDate = 16-06-2021 00:00:00, GraduationDate = 16-06-2022 00:00:00, Status = Dropout }");
     }
 
     [Fact]
@@ -23,19 +23,19 @@ public class ImmutableStudentTests
     {
         //Arrange
         var student = new ImmutableStudent(
-            id: 1,
-            givenname: "Jens",
-            surname: "Jensen",
-            startdate: new DateTime(2019, 8, 23),
-            enddate: new DateTime(2021, 6, 16),
-            graduationdate: new DateTime(2022, 6, 16)
+            1,
+            "Jens",
+            "Jensen",
+            new DateTime(2019, 8, 23),
+            new DateTime(2021, 6, 16),
+            new DateTime(2022, 6, 16)
         );
 
         var student2 = student with { };
         var equality = Object.Equals(student, student2);
         var referenceequality = Object.ReferenceEquals(student, student2);
-        
         //Assert
         equality.Should().BeTrue();
+        referenceequality.Should().BeFalse();
     }
 }
--- Assignment2/ImmutableStudent.cs
+++ Assignment2/ImmutableStudent.cs
@@ -1,20 +1,9 @@
 namespace Assignment2;
 
-public record ImmutableStudent
+public record ImmutableStudent(int Id, string GivenName, string Surname, DateTime StartDate, DateTime EndDate, DateTime GraduationDate)
 {
-    public int Id { get; init; }
-    public string GivenName { get; init; } = string.Empty;
-    public string Surname { get; init; } = string.Empty;
-    public Status Status { get; init; }
-    public DateTime StartDate { get; init; }
-    public DateTime EndDate { get; init; }
-    public DateTime GraduationDate { get; init; }
-
-    public ImmutableStudent(int id, string givenname, string surname, DateTime startdate, DateTime enddate, DateTime graduationdate)
-    {
-        (Id, GivenName, Surname, StartDate, EndDate, GraduationDate) = (id, givenname, surname, startdate, enddate, graduationdate);
-        Status = FindStatus(DateTime.Now);
-    }
+    public Status Status { get=> FindStatus(DateTime.Now); }
+    
     private Status FindStatus(DateTime t)
     {
         if (GraduationDate == EndDate && GraduationDate < t) return Status.Graduated;
