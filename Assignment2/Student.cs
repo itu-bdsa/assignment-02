27af6819165ee2d7c7764afe4af40cc3be34f23a
Clara Lyngeraa <clara.lyngeraa@gmail.com>
2022-09-20 16:33:14 +0200

    added witches

--- Assignment2/Student.cs
+++ Assignment2/Student.cs
@@ -4,7 +4,6 @@ public class Student
     public string givenName { get; set;} = null!;
     string surname { get; set;} = null!;
     DateTime startDate { get; set;} 
-
     DateTime endDate;
     DateTime graduationDate;
     Status status;
