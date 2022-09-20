883c1f139a40cfbe1e4d877e99687de4402444e2
Thomas Sørup Bavnhøj <tbav@itu.dk>
2022-09-20 18:02:29 +0200

    Added Flatten() test

--- Assignment2.Tests/ExtensionsTests.cs
+++ Assignment2.Tests/ExtensionsTests.cs
@@ -2,9 +2,31 @@ namespace Assignment2.Tests;
 
 public class ExtensionsTests
 {
-    // [Fact]
-    // public void Test1()
-    // {
+    [Fact]
+    public void Flatten_ListWithList_1_2_3_4_5__1_2_3_4()
+    {
+        var list = new List<List<int>>()
+        {
+            new List<int>() { 1, 2, 3, 4, 5 },
+            new List<int>() { 1, 2, 3, 4 }
+        };
+        var expected = new List<int>() { 1, 2, 3, 4, 5, 1, 2, 3, 4 };
+        var actual = list.Flatten();
+        actual.Should().BeEquivalentTo(expected);
+    }
+
+    [Fact]
+    public void Flatten_ListWithList_1_2_3_4_5__1_2_3_4_5()
+    {
+        var list = new List<List<int>>()
+        {
+            new List<int>() { 1, 2, 3, 4, 5 },
+            new List<int>() { 1, 2, 3, 4, 5 }
+        };
+        var expected = new List<int>() { 1, 2, 3, 4, 5, 1, 2, 3, 4, 5 };
+        var actual = Extensions.Flatten(list);
+        actual.Should().BeEquivalentTo(expected);
+    }
 
     // }
 }
