c7420c45330b6b6c17dc8f46b07ade65fd79ca8c
Clara Lyngeraa <clara.lyngeraa@gmail.com>
2022-09-20 12:26:51 +0200

    set to nullable

--- Assignment2/Student.cs
+++ Assignment2/Student.cs
@@ -1,9 +1,10 @@
 public class Student
 {
     int id { get; set;}
-    public string givenName{ get; set;}
-    string surname; //{ get; set;} 
+    public string givenName { get; set;} = null!;
+    string surname { get; set;} = null!;
     DateTime startDate { get; set;} 
+
     DateTime endDate;
     DateTime graduationDate;
     Status status;
