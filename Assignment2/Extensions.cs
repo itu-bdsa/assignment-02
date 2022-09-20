d60b66ddcc00ab7719e61a36b36c4b60bce07aa2
Thomas Sørup Bavnhøj <tbav@itu.dk>
2022-09-20 18:03:32 +0200

    Added Filter method

--- Assignment2/Extensions.cs
+++ Assignment2/Extensions.cs
@@ -3,4 +3,5 @@ namespace Assignment2;
 public static class Extensions
 {
     public static IEnumerable<T> Flatten<T>(this IEnumerable<IEnumerable<T>> source) => source.SelectMany(x => x).ToList();
+    public static IEnumerable<T> Filter<T>(this IEnumerable<T> source, Func<T, bool> filter) => source.Where(filter);
 }
