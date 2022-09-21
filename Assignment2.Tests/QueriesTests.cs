b0b76dad25566b543563b9a669de92505e80b601
Maxime Birkkjær Havez <maxih@hotmail.dk>
2022-09-21 18:13:34 +0200

    Made linq queries

--- Assignment2.Tests/QueriesTests.cs
+++ Assignment2.Tests/QueriesTests.cs
@@ -3,8 +3,51 @@ namespace Assignment2.Tests;
 public class QueriesTests
 {
     [Fact]
-    public void Test1()
+    public void Get_Names_Where_Creator_Like_Rowling()
     {
+        var expected = new[]{"Harry Potter","Voldemort","Albus Dumbledore"};
 
+
+        var resultExtensions = Queries.GetNamesByCreatorUsingExtensions("Rowling");
+        var resultLinq = Queries.GetNamesByCreatorUsingLinq("Rowling");
+
+        resultExtensions.Should().BeEquivalentTo(expected);
+        resultLinq.Should().BeEquivalentTo(expected);
+    }
+
+    [Fact]
+    public void Get_Sith_lords()
+    {
+        var resultExtensions = Queries.FindYearsOfSithUsingExtensions();
+        var resultLinq = Queries.FindYearsOfSithUsingLinq();
+
+        resultExtensions.Should().BeEquivalentTo(new[]{1977});
+        resultLinq.Should().BeEquivalentTo(new[]{1977});
+
+    }
+    [Fact]
+    public void Year_and_Name_from_Harry_Potter_books()
+    {
+        var expected = new[]{("Harry Potter",1997),("Albus Dumbledore",1997),("Voldemort",1997)};
+
+        var resultExtensions = Queries.WizardsInHarryPotterUsingExtensions();
+        var resultLinq = Queries.WizardsInHarryPotterUsingLinq();
+
+        resultExtensions.Should().BeEquivalentTo(expected);
+        resultLinq.Should().BeEquivalentTo(expected);
+    }
+
+    [Fact]
+    public void Group_by_reverse_Creator()
+    {
+        // Given
+        var expected = new[]{"Glinda the Good Witch","Stephen Strange","Willow Rosenberg","Jafar","Merlin",
+        "Gandalf","Sauron","Albus Dumbledore", "Harry Potter","Voldemort","Darth Vader","Zatanna"};
+        // When
+        var resultExtensions = Queries.NamesSortedInReverseByCreatorExtensions();
+        var resultLinq = Queries.NamesSortedInReverseByCreatorLinq();
+        // Then
+        Assert.Equal(expected,resultExtensions);
+        Assert.Equal(expected,resultLinq);
     }
 }
--- Assignment2/Queries.cs
+++ Assignment2/Queries.cs
@@ -1,5 +1,62 @@
 namespace Assignment2;
+using System.Linq;
 
 public class Queries
 {
+    public static IEnumerable<string> GetNamesByCreatorUsingExtensions(string creatorName){
+        var wizCol = WizardCollection.Create();
+        return wizCol.Where(w => w.Creator.Contains(creatorName)).Select(w=>w.Name);
+    }
+
+    public static IEnumerable<string> GetNamesByCreatorUsingLinq(string creator){
+        var wizards = WizardCollection.Create();
+        return from w in wizards
+         where w.Creator.Contains(creator)
+         select w.Name;
+    }
+
+    public static IEnumerable<int?> FindYearsOfSithUsingExtensions(){
+        var wizards = WizardCollection.Create();
+        return wizards.Where(w => w.Name.Contains("Darth")).Select(w=>w.Year);
+    }
+
+    public static IEnumerable<int?> FindYearsOfSithUsingLinq(){
+        var wizards = WizardCollection.Create();
+        return from w in wizards
+        where w.Name.Contains("Darth")
+        select w.Year;
+    }
+
+    public static IEnumerable<(string,int?)> WizardsInHarryPotterUsingExtensions(){
+        var wizards = WizardCollection.Create();
+        return wizards.Where(w => w.Medium.Contains("Harry Potter")).Select(w=>(w.Name,w.Year));
+    }
+
+    public static IEnumerable<(string,int?)> WizardsInHarryPotterUsingLinq(){
+        var wizards = WizardCollection.Create();
+        return from w in wizards
+        where w.Medium.Contains("Harry Potter")
+        select (w.Name,w.Year);
+    }
+
+    public static IEnumerable<string> NamesSortedInReverseByCreatorExtensions(){
+        var wizards = WizardCollection.Create();
+
+        return wizards.GroupBy(w => w.Creator).OrderByDescending(g => g.Key).Select(g => g.OrderBy(w => w.Name)).SelectMany(group=>group).Select(w=>w.Name);
+
+    }
+
+    public static IEnumerable<string> NamesSortedInReverseByCreatorLinq(){
+        var wizards = WizardCollection.Create();
+
+        var grouped = from w in wizards
+        group w by w.Creator into g
+        orderby g.Key descending
+        select g.OrderBy(wiz=>wiz.Name);
+
+        return from w in grouped.SelectMany(g=>g)
+        select w.Name;
+
+    }
+
 }
