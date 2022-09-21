37c02bee69fde7cfb0219d327945b95a2e3f4185
lukas <lanr@itu.dk>
2022-09-21 16:06:29 +0200

    LINQ done

--- Assignment2.Tests/WizardTests.cs
+++ Assignment2.Tests/WizardTests.cs
@@ -7,7 +7,7 @@ public class WizardTests
     {
         var wizards = WizardCollection.Create();
 
-        Assert.Equal(2, wizards.Count());
+        Assert.Equal(12, wizards.Count());
     }
 
     [Theory]
@@ -18,6 +18,58 @@ public class WizardTests
         var wizards = WizardCollection.Create();
 
         Assert.Contains(wizards, w => w == new Wizard(name, medium, year, creator));
+    }
+
+    [Fact]
+    public void RowlingWizards()
+    {
+        var wizards = WizardCollection.Create();
+        var rowling1 = Queries.RowlingWizardsLinq(wizards);
+        var rowling2 = Queries.RowlingWizards(wizards);
+
+        var expected = new[] { "Harry Potter" };
+
+        Assert.Equal(expected, rowling1);
+        Assert.Equal(expected, rowling2);
+    }
+
+    [Fact]
+    public void FirstSithLord()
+    {
+        var wizards = WizardCollection.Create();
+        var sith1 = Queries.TheYearOfTheFirstSithLordLinq(wizards);
+        var sith2 = Queries.TheYearOfTheFirstSithLord(wizards);
+
+        var expected = 1977;
+
+        Assert.Equal(expected, sith1);
+        Assert.Equal(expected, sith2);
+    }
+
+    [Fact]
+    public void HarryPotterWizards()
+    {
+        var wizards = WizardCollection.Create();
+        var harry1 = Queries.HarryWizardsLinq(wizards);
+        var harry2 = Queries.HarryWizards(wizards);
+
+        var expected = new(string, int?)[] {("Harry Potter", 2010)};
+
+        Assert.Equal(expected, harry1);
+        Assert.Equal(expected, harry2);
+    }
+
+    [Fact]
+    public void GroupedWizards()
+    {
+        var wizards = WizardCollection.Create();
+        var group1 = Queries.WizardsGroupLinq(wizards);
+        var group2 = Queries.WizardsGroup(wizards);
+
+        var expected = new[] { "The Time Wizard", "Spider-man", "Gandalf", "Sauron", "Harry Potter", "Wizard 1", "Wizard 2", "Wizard 3", "Wizard 4", "Wizard 5", "Wizard 6", "Darth Vader" };
+
+        Assert.Equal(expected, group1);
+        Assert.Equal(expected, group2);
     }
 }
 
--- Assignment2/Queries.cs
+++ Assignment2/Queries.cs
@@ -1,20 +1,60 @@
 using Microsoft.VisualBasic;
+using System.ComponentModel;
 
 namespace Assignment2;
 
 public class Queries
 {
     public static IEnumerable<string> RowlingWizardsLinq(IEnumerable<Wizard> wizards) => wizards.Where(a => a.Creator == "J.K. Rowling").Select(a => a.Name);
-    public static IEnumerable<string> RowlingWizards(IEnumerable<Wizard> wizards) => wizards.Where(a => a.Creator == "J.K. Rowling").Select(a => a.Name);
+    public static IEnumerable<string> RowlingWizards(IEnumerable<Wizard> wizards)
+    {
+        return (from c in wizards
+                where c.Creator == "J.K. Rowling"
+                select new
+                {
+                    Name = c.Name,
+                }.Name).ToList();
+    }
 
 
-    public static int? TheYearOfTheFifthSithLordLinq(IEnumerable<Wizard> wizards) => wizards.Where(a => a.Name.Contains("Darth")).Select(a => a.Year).First();
-    public static int? TheYearOfTheFifthSithLord(IEnumerable<Wizard> wizards) => wizards.Where(a => a.Name.Contains("Darth")).Select(a => a.Year).First();
+    public static int? TheYearOfTheFirstSithLordLinq(IEnumerable<Wizard> wizards) => wizards.Where(a => a.Name.Contains("Darth")).OrderBy(a => a.Year).Select(a => a.Year).First();
+    public static int? TheYearOfTheFirstSithLord(IEnumerable<Wizard> wizards)
+    {
+        return (from c in wizards
+                where c.Name.Contains("Darth")
+                select new
+                {
+                    Year = c.Year
+                }.Year).First();
+    }
 
 
-    public static IEnumerable<(string, int?)> HarryWizardsLinq(IEnumerable<Wizard> wizards) => wizards.Where(a => a.Medium.Contains("Harry Potter")).Select(a => (a.Name, a.Year));
-    public static IEnumerable<(string, int?)> HarryWizards(IEnumerable<Wizard> wizards) => wizards.Where(a => a.Medium.Contains("Harry Potter")).Select(a => (a.Name, a.Year));
+    public static IEnumerable<(string, int?)> HarryWizardsLinq(IEnumerable<Wizard> wizards) => wizards.Where(a => a.Medium.Contains("Harry Potter")).Select(a => (a.Name, a.Year)).Distinct();
+    public static IEnumerable<(string, int?)> HarryWizards(IEnumerable<Wizard> wizards)
+    {
+        var wiz = (from c in wizards
+                   where c.Medium.Contains("Harry Potter")
+                   select new
+                   {
+                       Name = c.Name,
+                       Year = c.Year,
+                   }).Distinct();
 
-    public static IEnumerable<Wizard> WizardsGroupLinq(IEnumerable<Wizard> wizards) => null; //wizards.OrderByDescending(a => a.Creator).ThenBy(a => a.Name).GroupBy(a => a.Creator).Select(a => a);
+        foreach (var w in wiz)
+        {
+            yield return (w.Name, w.Year);
+        }
+    }
 
+    public static IEnumerable<String> WizardsGroupLinq(IEnumerable<Wizard> wizards) => wizards.OrderByDescending(a => a.Creator).ThenBy(a => a.Name).Select(a => a.Name);
+    public static IEnumerable<String> WizardsGroup(IEnumerable<Wizard> wizards)
+    {
+        return (from c in wizards
+                orderby c.Name
+                orderby c.Creator descending
+                select new
+                {
+                    c.Name,
+                }.Name).ToList();
+    }
 }
