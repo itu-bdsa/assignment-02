511db6bca2a5eb45bb2e20fc8272416c92d21425
oljh <oljh@itu.dk>
2022-09-21 16:38:54 +0200

    whitespace

--- Assignment2/ImmutableStudent.cs
+++ Assignment2/ImmutableStudent.cs
@@ -2,8 +2,8 @@ namespace Assignment2;
 
 public record ImmutableStudent(int Id, string GivenName, string Surname, DateTime StartDate, DateTime EndDate, DateTime GraduationDate)
 {
-    public Status Status { get=> FindStatus(DateTime.Now); }
-    
+    public Status Status { get => FindStatus(DateTime.Now); }
+
     private Status FindStatus(DateTime t)
     {
         if (GraduationDate == EndDate && GraduationDate < t) return Status.Graduated;
