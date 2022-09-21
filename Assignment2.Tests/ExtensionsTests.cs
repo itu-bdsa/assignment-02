65781edcf1069e99158bd1d794004ec44daeef36
Elisabeth <elbr@itu.dk>
2022-09-21 17:05:33 +0200

    Done with extensions and extensions tests

--- Assignment2.Tests/ExtensionsTests.cs
+++ Assignment2.Tests/ExtensionsTests.cs
@@ -5,6 +5,19 @@ public class ExtensionsTests
     [Fact]
     public void Test1()
     {
+        Uri uri=new Uri("https://www.youtube.com/");
+        var test1=Extensions.isSecure(uri);
+
+        test1.Should().BeTrue();
 
     }
+
+    [Fact]
+    public void test2(){
+        String s= new String ("Hello World I am very happy :3");
+        var test2=Extensions.wordCount(s);
+
+        test2.Should().Be(6);
+    }
 }
+
--- Assignment2/Extensions.cs
+++ Assignment2/Extensions.cs
@@ -2,4 +2,22 @@ namespace Assignment2;
 
 public static class Extensions
 {
+
+public static bool isSecure(this Uri uri){ 
+return uri.Scheme==Uri.UriSchemeHttps;
+}
+
+public static int wordCount(this String count){
+    bool isLetter=count.All(Char.IsLetter);
+        int c=0;
+
+        for(int i=0; i<count.Length-1; i++){
+            if(count[i]==' ' && char.IsLetter(count[i+1]) && (i>0)){
+                c++;
+            }
+        }
+        c++;
+        return c;
+
+}
 }
