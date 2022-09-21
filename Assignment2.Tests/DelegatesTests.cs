0173542b8ced4cf5b8b3991c4b958592c26cb1ff
Una Mathiesen Langeland <unla@itu.dk>
2022-09-21 09:42:24 +0200

    Added delegates and delegatestests

--- Assignment2.Tests/DelegatesTests.cs
+++ Assignment2.Tests/DelegatesTests.cs
@@ -14,7 +14,44 @@ public class DelegatesTests
     [Fact]
     public void lambda_given_string_sentence_returns_reverse()
     {
-        var s = "Hej";
-        Assert.Equal("jeH", Delegates.stringReverseLambda(s));
+        var s = "Hej med deg";
+        Assert.Equal("ged dem jeH", Delegates.stringReverseLambda(s));
     }
+
+    [Fact]
+    public void lambda_given_two_deicmals_return_product()
+    {
+        var a = 5.5;
+        var b = 6.6321;
+
+        Assert.Equal(a + b, Delegates.DecimalsProductLambda(a, b));
+    }
+
+    [Fact]
+    public void lambda_given_0042_and_42_return_true()
+    {
+        var a = 42;
+        var b = "0042";
+
+        Assert.True(Delegates.EqualLambda(a, b));
+    }
+
+    [Fact]
+    public void lambda_given_420_and_0420_return_true()
+    {
+        var a = 420;
+        var b = "0420";
+
+        Assert.True(Delegates.EqualLambda(a, b));
+    }
+
+    [Fact]
+    public void lambda_given_66_and_33_return_false()
+    {
+        var a = 66;
+        var b = "33";
+
+        Assert.False(Delegates.EqualLambda(a, b));
+    }
+
 }
--- Assignment2/Delegates.cs
+++ Assignment2/Delegates.cs
@@ -4,7 +4,15 @@ public class Delegates
 {
 
     public static reverse stringReverse = new reverse(x => new String(x.ToCharArray().Reverse().ToArray()));
-    public static Func<string,string> stringReverseLambda = (x => new String(x.ToCharArray().Reverse().ToArray()));
+    public static Func<string, string> stringReverseLambda = (x => new String(x.ToCharArray().Reverse().ToArray()));
 
     public delegate string reverse(string s);
+
+    public static TwoDecimals DecimalsProduct = (a, b) => a + b;
+    public static Func<double, double, double> DecimalsProductLambda = ((a, b) => a + b);
+    public delegate double TwoDecimals(double a, double b);
+
+    public static EqualNumbers EqualProduct = ((a, b) => a.ToString() == b.TrimStart('0'));
+    public static Func<int, string, bool> EqualLambda = ((a, b) => a.ToString() == b.TrimStart('0'));
+    public delegate bool EqualNumbers(int a, string b);
 }
